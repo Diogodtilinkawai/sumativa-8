b28aa166b2eb79547f0bb917bfe76043ff079642
Donvicio <10143@portalcedi.edu.mx>
2025-01-21 11:02:13 -0600

    sumativa 8 final

--- sumativa-8/src/app/home/home.page.ts
+++ sumativa-8/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,28 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  nombre = new FormControl('');
+  correo = new FormControl('');
+  items: any = [];
 
-  constructor() {}
+  agregar() {
+    if (this.nombre.value && this.correo.value) {
+      this.items.push({ nombre: this.nombre.value, correo: this.correo.value });
+      this.nombre.setValue('');
+      this.correo.setValue('');
+    }
+  }
+  eliminar(indice: any) {
+    this.items.splice(indice, 1);
+  }
+  editar(indice: any) {
+    if (this.nombre.value && this.correo.value) {
+      this.items[indice].nombre = this.nombre.value;
+      this.items[indice].correo = this.correo.value;
+      this.nombre.setValue('');
+      this.correo.setValue('');
+    }
+  }
+  constructor() { }
 
 }
